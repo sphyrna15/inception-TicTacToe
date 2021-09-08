b004b07538b0774c530db5f1daf31ae2d9fb0b41
sphyrna15 <timmysnakes@gmail.com>
2021-09-08 13:50:13 +0200

    Driver to allow gameplay vs computer

---
+++ inceptionBrain.py
@@ -0,0 +1,14 @@
+"""
+Inception Tic-Tac-Toe 
+
+Driver to Play against Computer program
+"""
+
+import pygame as p
+import numpy as np
+
+# Import modules
+from inceptionMain import draw_lines
+from inceptionMain import draw_letters
+from inceptionMain import draw_shellLetters
+from inceptionMain import draw_winLines
